b4b67e91f43c3fd37eb08282604f14af912740bd
Никита <nik.oschepkov@yandex.ru>
2020-11-29 00:06:03 +0300

    little of everything

--- game/Class1.cs
+++ game/Class1.cs
@@ -16,85 +16,304 @@ namespace game
 
     public class GameObj
     {
-        public int type;
+        public enum typeObj
+        {
+            empty,
+            block,
+            unit,
+            mine,
+            town
+        }
+        public typeObj type;
         public Coord Position;
-        public int Player;
+        public Player owner;
+        public int health;
     }
 
-    public class Unit: GameObj
+    // Units //
+    abstract public class Unit: GameObj
     {
-        public Player player;
+        public enum typeUnit
+        {
+            Scout,
+            Warior,
+            Shooter,
+            Top
+        }
         public int attack;
         public int defense;
         public int damage;
-        public int health;
         public int actionPoints;
+
+        public int MAXactionPoints;
+        abstract public void atack(GameObj unit);
     }
 
-    public class Cave: GameObj
+    public class Scout: Unit
     {
+        public Scout()
+        {
+            this.attack = 1;
+            this.defense = 1;
+            this.damage = 1;
+            this.health = 1;
+            this.actionPoints = 1;
+        }
+        override public void atack(GameObj unit)
+        {
 
+        }
+    }
+
+    public class Warior : Unit
+    {
+        public int level;
+        public int rangeAttack;
+        public int shootingDamage;
+        public Warior(int level)
+        {
+            this.level = level;
+            switch (level)
+            {
+                case 1:
+                    this.attack         = 1;
+                    this.defense        = 1;
+                    this.damage         = 1;
+                    this.health         = 1;
+                    this.actionPoints   = 1;
+                    break;
+                case 2:
+                    this.attack         = 2;
+                    this.defense        = 2;
+                    this.damage         = 2;
+                    this.health         = 2;
+                    this.actionPoints   = 2;
+                    break;
+                default:
+                    this.attack         = 3;
+                    this.defense        = 3;
+                    this.damage         = 3;
+                    this.health         = 3;
+                    this.actionPoints   = 3;
+                    break;
+            }
+        }
+        override public void atack(GameObj unit)
+        {
+
+        }
+    }
+
+    public class Shooter : Unit
+    {
+        public int level;
+        public Shooter(int level)
+        {
+            this.level = level;
+            switch (level)
+            {
+                case 1:
+                    this.attack = 1;
+                    this.defense = 1;
+                    this.damage = 1;
+                    this.health = 1;
+                    this.actionPoints = 1;
+                    break;
+                case 2:
+                    this.attack = 2;
+                    this.defense = 2;
+                    this.damage = 2;
+                    this.health = 2;
+                    this.actionPoints = 2;
+                    break;
+                default:
+                    this.attack = 3;
+                    this.defense = 3;
+                    this.damage = 3;
+                    this.health = 3;
+                    this.actionPoints = 3;
+                    break;
+            }
+        }
+        override public void atack(GameObj unit)
+        {
+
+        }
+    }
+
+    public class Top : Unit
+    {
+        public Top()
+        {
+            this.attack = 1;
+            this.defense = 1;
+            this.damage = 1;
+            this.health = 1;
+            this.actionPoints = 1;
+        }
+        override public void atack(GameObj unit)
+        {
+
+        }
+    }
+
+    // ... //
+
+    public class Mine: GameObj
+    {
+        public enum typeMine
+        {
+            gold,
+            wood,
+            rock,
+            crystall
+        }
+        public typeMine TypeMine;
+        public void mining()
+        {
+            switch (TypeMine)
+            {
+                case typeMine.gold:
+                    this.owner.gold += 1;
+                    break;
+                case typeMine.wood:
+                    this.owner.wood += 1;
+                    break;
+                case typeMine.rock:
+                    this.owner.rock += 1;
+                    break;
+                case typeMine.crystall:
+                    this.owner.crystall += 1;
+                    break;
+            }
+        }
     }
 
     public class Town: GameObj
     {
-
+        public int level;
+        public void upgrade()
+        {
+            level++;
+            if (level == 2) health += 2;
+            else if (level == 3) health += 3;
+            else throw new Exception("Max upgrade");
+        }
     }
 
-    public class Market: GameObj
-    {
-
-    }
     public class _Map
     {
-        public GameObj[,] Map { get; set; }
-        _Map(int X = 71, int Y = 51)
+        public GameObj[,] Map;
+        public Coord[,] SpawnPos; 
+        public Town[] Towns;
+        public Mine[] Mines;
+        public _Map(int X = 71, int Y = 51) // expecting 70 X 50 size map
         {
             Map = new GameObj[X, Y];
             for (int i = 0; i < X; i++)
                 for (int j = 0; j < Y; j++)
                     Map[i, j].Position = new Coord { X = i, Y = j};
         }
+        public void InitGame(Player[] players)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                players[i].town = Towns[i];
+                players[i].gold = 0;
+                players[i].rock = 0;
+                players[i].wood = 0;
+                players[i].crystall = 0;
+                players[i].id = i;
+            }            
+        }
+
+        public Coord SpawnUnit(Unit u)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Coord c = SpawnPos[u.owner.id, i];
+                if (Map[c.X, c.Y].type == GameObj.typeObj.empty)
+                {
+                    Map[c.X, c.Y] = u;
+                    return c;
+                }
+            }
+            throw new Exception("no free places");
+        }
     }
 
     public class Player
     {
+        public string name;
+        public int id;
+        public int gold, rock, wood, crystall;
         public Unit selectUnit;
+        public Town town;
+        public Player(string name)
+        {
+            this.name = name;            
+        }
     }
 
     interface IAction
     {
         static Player player;
         static _Map map;
-
         static public void SelectUnit(Unit unit) // click (Left)
         {
+            if (unit.owner != player) throw new Exception("This is enemy");
             player.selectUnit = unit;
         }
-        static public void MoveUnit()  // click (Left)
+        static Coord[] MoveUnit()  // click (Left)
         {
-
+            if (player.selectUnit == null) throw new Exception("Not select unit");
+            // SANYA // 
+            int costMoving = 0;
+            if (player.selectUnit.actionPoints - costMoving < 0) throw new Exception("Not moving");
+            return null; 
         }
-        static public void AttackUnit() // click (Right)
+        static public GameObj[] Attack(GameObj obj) // click (Right)
         {
-
+            if (player.selectUnit == null) throw new Exception("Not select unit");
+            player.selectUnit.atack(obj);
+            return new GameObj[] { player.selectUnit, obj };
         }
 
-        static public void SpawnUnit(int id) // interface buttons
-        { 
-
+        static public Coord SpawnUnit(Unit.typeUnit id, int level = 1) // interface buttons
+        {
+            Unit u = null;
+            switch (id)
+            {
+                case Unit.typeUnit.Scout:
+                    u = new Scout();
+                    break;
+                case Unit.typeUnit.Warior:
+                    u = new Warior(level);
+                    break;
+                case Unit.typeUnit.Shooter:
+                    u = new Shooter(level);
+                    break;
+                case Unit.typeUnit.Top:
+                    u = new Top();
+                    break;
+            }
+            u.owner = player;
+            return map.SpawnUnit(u);
         } 
-        static public void UpgradeTown(int id) // interface buttons
+        static public void UpgradeTown() // interface buttons
         {
-
+            player.town.upgrade();
         }
-        static public void Market() 
+        static public void Market() // 
         { 
 
         }
-        static public void CaptureCave() 
-        { 
-
+        static public void CaptureMine(Mine mine) // click (Right)
+        {
+            int X = Math.Abs(player.selectUnit.Position.X - mine.Position.X);
+            int Y = Math.Abs(player.selectUnit.Position.Y - mine.Position.Y);
+            if (X > 1 || Y > 1) throw new Exception("out of range");
+            mine.owner = player;            
         }
     }
 
@@ -112,22 +331,22 @@ namespace game
         }
 
 
-        Game(_Map map)
+        Game(_Map map, Player[] p) // expecting 2 players
         {
             this.map = map;
-            IAction.map = map;
-            players = new Player[2];
+            players = p;
+            map.InitGame(players);
+            IAction.player = players[0];
         }
 
-        public void Action(Buttons button, int param, Coord pos)
-        {
+        public object Action(Buttons button, int param, Coord pos)
+        {   
             switch (button)
             {
                 case Buttons.SpawnUnit:
-                    IAction.SpawnUnit(param);
-                    break;
+                    return IAction.SpawnUnit((Unit.typeUnit)param);
                 case Buttons.UpgradeTown:
-                    IAction.UpgradeTown(param);
+                    IAction.UpgradeTown();
                     break;
                 case Buttons.NextTurn:
                     nextTurn();
@@ -136,11 +355,21 @@ namespace game
                 case Buttons.Right:
                     switch (map.Map[pos.X, pos.Y].type)
                     {
-
+                        case GameObj.typeObj.empty when button == Buttons.Left:
+                            return IAction.MoveUnit(); 
+                        case GameObj.typeObj.unit when button == Buttons.Left:
+                            IAction.SelectUnit((Unit)map.Map[pos.X, pos.Y]);
+                            break;
+                        case GameObj.typeObj.unit when button == Buttons.Right:
+                        case GameObj.typeObj.town when button == Buttons.Right:
+                            return IAction.Attack(map.Map[pos.X, pos.Y]); 
+                        case GameObj.typeObj.mine when button == Buttons.Right:
+                            IAction.CaptureMine((Mine)map.Map[pos.X, pos.Y]);
+                            break;
                     }
                     break;
             }
-
+            return null;
         }
 
         public void nextTurn() {
